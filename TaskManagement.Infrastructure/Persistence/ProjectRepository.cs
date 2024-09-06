7801f7adb4ecaeb7ecdc828e2bbd362e384eb392
leandro.boas <leandro.boas@ltm.digital>
2024-09-06 02:06:46 -0300

    criando as classes e algumas implementações

---
+++ TaskManagement.Infrastructure/Persistence/ProjectRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectManagement.Domain.Entities;
+using ProjectManagement.Domain.Repositories;
+
+namespace TaskManagement.Infrastructure.Persistence;
+
+public class ProjectRepository : IProjectRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public ProjectRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CreateAsync(Project project)
+    {
+        _context.Projects.Add(project);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<Project> GetByIdAsync(Guid id)
+    {
+        return await _context.Projects.Include(p => p.Tasks).FirstOrDefaultAsync(p => p.Id == id);
+    }
+
+    // Other methods...
+}
